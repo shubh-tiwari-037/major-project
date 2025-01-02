3eb5eedf4fb76b3cc69de3bd4de649c5b714c1a2
shubh-tiwari-037 <shubhtiwari037@gmail.com>
2025-01-02 12:46:43 +0530

    authentication

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const ejsMate= require("ejs-mate");
 const ExpressError= require("./utils/ExpressError.js");
 const session=require("express-session");
 const flash = require("connect-flash");
+const passport= require("passport");
+const LocalStrategy = require("passport-local");
+const User = ("./models/user.js");
 // const {listingSchema,reviewSchema}=require("./schema.js");
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -57,15 +61,34 @@ res.send("hi i am root ")
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     // console.log(  res.locals.success);
     next();
+});
+
+app.get("/demouser",async (req,res)=>{
+    let fakeUser = new User({
+        email : "student@gmail.com",
+        username :"delta-student",
+    });
+
+    let registeredUser = await User.register(fakeUser,"helloworld");
+    res.send(registeredUser);
 })
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 // const validateListing= (req,res,next)=>{
 //     let {error} =listingSchema.validate(req.body);
